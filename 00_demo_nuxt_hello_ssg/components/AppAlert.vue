fc1b5b241b9af9421ae4533f4dd44f0abc664a5a
Rémi Connesson <remiconnesson@gmail.com>
2022-10-11 16:31:50 +0200

    Demonstrate auto-imports of components

---
+++ 00_demo_nuxt_hello_ssg/components/AppAlert.vue
@@ -0,0 +1,6 @@
+<template>
+  <h2>Ceci est un composant</h2>
+  <span>
+    <slot />
+  </span>
+</template>
---
+++ 00_demo_nuxt_hello_ssg/pages/404.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>404 page</h1>
+  <AppAlert>
+    <p>Vous ne passerez pas</p>
+  </AppAlert>
+</template>
